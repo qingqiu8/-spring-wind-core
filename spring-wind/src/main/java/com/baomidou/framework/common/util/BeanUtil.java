4cf5e319d3d15b625cbb4d07fd1eef41650c0a31
青苗 <=>
2016-08-24 21:45:42 +0800

    修改优化内容

--- spring-wind/src/main/java/com/baomidou/framework/common/util/BeanUtil.java
+++ spring-wind/src/main/java/com/baomidou/framework/common/util/BeanUtil.java
@@ -15,11 +15,17 @@
  */
 package com.baomidou.framework.common.util;
 
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.FatalBeanException;
 import org.springframework.cglib.beans.BeanCopier;
 import org.springframework.cglib.beans.BeanMap;
+import org.springframework.util.Assert;
 
 /**
  * <p>
@@ -35,72 +41,105 @@ public final class BeanUtil extends BeanUtils {
 		/* 保护 */
 	}
 
-
 	/**
 	 * 实例化对象
-	 * @param clazz 类
+	 * 
+	 * @param clazz
+	 *            类
 	 * @return 对象
 	 */
 	@SuppressWarnings("unchecked")
-	public static <T> T newInstance( Class<?> clazz ) {
+	public static <T> T newInstance(Class<?> clazz) {
 		try {
 			return (T) clazz.newInstance();
-		} catch ( InstantiationException e ) {
+		} catch (InstantiationException e) {
 			throw new RuntimeException(e);
-		} catch ( IllegalAccessException e ) {
+		} catch (IllegalAccessException e) {
 			throw new RuntimeException(e);
 		}
 	}
-
 
 	/**
 	 * 实例化对象
-	 * @param clazzStr 类名
+	 * 
+	 * @param clazzStr
+	 *            类名
 	 * @return {T}
 	 */
-	public static <T> T newInstance( String clazzStr ) {
+	public static <T> T newInstance(String clazzStr) {
 		try {
 			Class<?> clazz = Class.forName(clazzStr);
 			return newInstance(clazz);
-		} catch ( ClassNotFoundException e ) {
+		} catch (ClassNotFoundException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-
 	/**
 	 * copy 对象属性到另一个对象，默认不使用Convert
+	 * 
 	 * @param src
-	 * @param clazz 类名
+	 * @param clazz
+	 *            类名
 	 * @return {T}
 	 */
-	public static <T> T copy( Object src, Class<T> clazz ) {
+	public static <T> T clone(Object src, Class<T> clazz) {
 		BeanCopier copier = BeanCopier.create(src.getClass(), clazz, false);
 		T to = newInstance(clazz);
 		copier.copy(src, to, null);
 		return to;
 	}
 
-
 	/**
 	 * 拷贝对象
-	 * @param src 源对象
-	 * @param dist 需要赋值的对象
+	 * 
+	 * @param source
+	 *            源对象
+	 * @param target
+	 *            需要赋值的对象
 	 */
-	public static void copy( Object src, Object dist ) {
-		BeanCopier copier = BeanCopier.create(src.getClass(), dist.getClass(), false);
-		copier.copy(src, dist, null);
+	public static void copy(Object source, Object target) throws BeansException {
+		Assert.notNull(source, "Source must not be null");
+		Assert.notNull(target, "Target must not be null");
+		Class<?> actualEditable = target.getClass();
+		PropertyDescriptor[] targetPds = getPropertyDescriptors(actualEditable);
+		for (PropertyDescriptor targetPd : targetPds) {
+			if (targetPd.getWriteMethod() != null) {
+				PropertyDescriptor sourcePd = getPropertyDescriptor(source.getClass(), targetPd.getName());
+				if (sourcePd != null && sourcePd.getReadMethod() != null) {
+					try {
+						Method readMethod = sourcePd.getReadMethod();
+						if (!Modifier.isPublic(readMethod.getDeclaringClass().getModifiers())) {
+							readMethod.setAccessible(true);
+						}
+						/*
+						 * no copy null properties
+						 */
+						Object value = readMethod.invoke(source);
+						if (value != null) {
+							Method writeMethod = targetPd.getWriteMethod();
+							if (!Modifier.isPublic(writeMethod.getDeclaringClass().getModifiers())) {
+								writeMethod.setAccessible(true);
+							}
+							writeMethod.invoke(target, value);
+						}
+					} catch (Throwable ex) {
+						throw new FatalBeanException("Could not copy properties from source to target", ex);
+					}
+				}
+			}
+		}
 	}
 
-
 	/**
-	 * 将对象装成map形式
-	 * 注意：生成的是unmodifiableMap
-	 * @param src 源对象
+	 * 将对象装成map形式 注意：生成的是unmodifiableMap
+	 * 
+	 * @param src
+	 *            源对象
 	 * @return {Map<K, V>}
 	 */
 	@SuppressWarnings("unchecked")
-	public static <K, V> Map<K, V> toMap( Object src ) {
+	public static <K, V> Map<K, V> toMap(Object src) {
 		return BeanMap.create(src);
 	}
 
